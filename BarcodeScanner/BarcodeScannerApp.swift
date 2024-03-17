656aedbffe50e47d0db45a37d917129ca1c0c30a
ikhsansyahrizal <ikhsansyahrizal23@gmail.com>
2024-03-17 10:44:10 +0700

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Ikhsan on 17/03/24.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
